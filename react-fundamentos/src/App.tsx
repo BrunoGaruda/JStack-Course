1c653bff0b4bc68ec1171efe72b38699ab241d90
BrunoGaruda <bruleorn@gmail.com>
2022-11-02 17:27:11 -0300

    criandod o estado usando useState, com conceito de imutabilidade, e renderizando os posts utilizando o spread com o array

--- react-fundamentos/src/App.tsx
+++ react-fundamentos/src/App.tsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Post from './Post'
 import Header from './Header'
 // const catergory = 'Posts da semana'
 
-const posts = [
-  { title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
-  { title: 'Title#01', subtitle: 'Sub#01', likes: 10 },
-  { title: 'Title#01', subtitle: 'Sub#01', likes: 50 }
-]
+// interface postsArray {
+//   id: string
+//   title: string
+//   subtitle: string
+//   likes: number
+// }
 
 // Props, vamos renderizar o conteudo do componente de forma dinâmica
 export function App() {
+  // Foi necessário a desestruturação do array para manter o principio de imutabilidade. O react trabalha com SPA e não é necessário a renderização do array, apenas do que foi alterado, o estado.
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
+    { id: 2, title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
+    { id: 3, title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
+    { id: 4, title: 'Title#04', subtitle: 'Sub#04', likes: 100 }
+  ])
+
+  function handleRefresh() {
+    // Método push copia o array e o altera. No react é diferente, não vai funcionar. React trabalha com valores imutáveis
+    // posts.push()
+
+    setPosts([
+      ...posts,
+      {
+        id: posts.length + 1,
+        title: `Title#0${posts.length + 1}`,
+        subtitle: `Sub#0${posts.length + 1}`,
+        likes: Math.random() * 1000
+      }
+    ])
+  }
+
   return (
     <>
       <Header>
@@ -18,14 +42,17 @@ export function App() {
           {/* tive que colocar uma div ao fragment parar concertar o erro 
           https://github.com/mbrn/material-table/issues/653
           */}
-          <h4>Subtítulo da semana 01</h4>
+          <h4>
+            Subtítulo da semana 01
+            <button onClick={handleRefresh}>Atualizar</button>
+          </h4>
         </div>
       </Header>
       <hr />
 
       {posts.map(posts => (
         <Post
-          key={posts.title} // Foi necessário atribuir a Key para retirar o erro, sempre que foi atribuir uma lista, tem q colorar a Key.
+          key={posts.id} // Foi necessário atribuir a Key para retirar o erro, sempre que foi atribuir uma lista, tem q colorar a Key.
           likes={posts.likes}
           post={{
             title: posts.title,
