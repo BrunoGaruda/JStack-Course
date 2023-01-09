cfd544140937929bc5d5cc85cc38f5c8d9adf06b
BrunoAlmeida <bruleorn@gmail.com>
2023-01-09 20:59:29 -0300

    retirando a / e '' com split, tornando o array com 2 elementos: users[0] e id[1] caso exista

--- node/src/index.js
+++ node/src/index.js
@@ -6,22 +6,34 @@ const routes = require('./routes')
 // criando servidor
 const server = http.createServer((request, response) => {
   const parsedUrl = new URL(`http://localhost:3000${request.url}`)
-
-  console.log(parsedUrl)
-
   // Método GET | Endpoint /
   console.log(
     `Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`
   )
 
+  let { pathname } = parsedUrl
+  let id = null
+
+  // Retirando a barra do endpoint com o split, e retirando o '' com o filter.
+  const splitEndPoint = pathname.split('/').filter(Boolean)
+  console.log(splitEndPoint)
+
+  // apenas 2 elementos do array, sendo o 1, o id
+  if (splitEndPoint.length > 1) {
+    pathname = `/${splitEndPoint[0]}/:id`
+    id = splitEndPoint[1]
+  }
+
   const route = routes.find(
     routeObj =>
-      routeObj.endpoint === parsedUrl.pathname &&
-      routeObj.method === request.method
+      routeObj.endpoint === pathname && routeObj.method === request.method
   )
 
   if (route) {
+    //Novo método de transformar em objeto os Query Params
     request.query = Object.fromEntries(parsedUrl.searchParams)
+    request.params = { id }
+
     route.handler(request, response)
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html' })
