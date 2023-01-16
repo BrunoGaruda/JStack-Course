3bed4235dee6a504f764dc6d97340bc22903cf18
BrunoAlmeida <bruleorn@gmail.com>
2023-01-16 01:16:40 -0300

    criando a primeira rota e os novos métodos do controller

---
+++ mycontacts/src/app/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+  index() {
+    // Listar todos os registros
+  }
+
+  show() {
+    // Obter UM registro
+  }
+
+  store() {
+    // Criar novo registro
+  }
+
+  update() {
+    // Editar um registro
+  }
+
+  delete() {
+    // Deletar um registro
+  }
+}
+// Singleton
+module.exports = new ContactController();
