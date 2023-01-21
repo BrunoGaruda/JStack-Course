1d355070af723cce77a475c3aed8b4369556e268
BrunoAlmeida <bruleorn@gmail.com>
2023-01-21 14:40:44 -0300

    criando o metodo POST, primeiro criando a rota, criando a regra de negócio no controller, e no repositório, criando a lógica dos metodos

--- mycontacts/src/app/controllers/ContactController.js
+++ mycontacts/src/app/controllers/ContactController.js
@@ -21,8 +21,30 @@ class ContactController {
     return response.json(contact);
   }
 
-  store() {
+  async store(request, response) {
     // Criar novo registro
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    // Nome é requirido para a validação
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    // Verificando se existe email cadastrado
+    const contactsExist = await ContactsRepository.findByEmail(email);
+
+    if (contactsExist) {
+      return response.status(400).json({ error: 'This e-mail already exists' });
+    }
+
+    // Criando
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id,
+    });
+
+    return response.json(contact);
   }
 
   update() {
--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -3,6 +3,17 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
+
+// // Middlewares
+// app.use((request, response) => {
+// // Middleware 1
+//   request.appId = 'MeuAppID';
+//   response.send('Interceptado pelo Middleware');
+// });
+
+// Middleware para ler requisições com body
+app.use(express.json());
+// Routes (Middleware)
 app.use(routes);
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
