80cdbc830a5398f1fcd9d996e159fde1a298136f
BrunoAlmeida <bruleorn@gmail.com>
2023-01-27 19:36:21 -0300

    refatorando o metodo findby id, email e id sendo incluindos ao banco de dados

--- mycontacts/src/repositories/ContactsRepository.js
+++ mycontacts/src/repositories/ContactsRepository.js
@@ -19,21 +19,23 @@ let contacts = [
   },
 ];
 
+// SELECT para pegar linha(s) do data, * para listar todas. FROM para localizar
+// WHERE(onde), quando o valor será igual ao bind ex: id = $1
+
 class ContactsRepository {
-  findAll() {
-    return new Promise((resolve) => resolve(contacts));
+  async findAll() {
+    const rows = await db.query('SELECT * FROM contacts');
+    return rows;
   }
 
-  findById(id) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.id === id),
-    ));
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+    return row;
   }
 
-  findByEmail(email) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.email === email),
-    ));
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email]);
+    return row;
   }
 
   delete(id) {
