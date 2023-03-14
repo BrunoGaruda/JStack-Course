6f53f6e1c7ab5a6afcbe0c78b171a9692cbe252c
BrunoAlmeida <bruleorn@gmail.com>
2023-03-14 17:11:25 -0300

    movendo app para components/app e renomeando para index

--- mycontacts/fe/src/components/App/index.js
+++ mycontacts/fe/src/components/App/index.js
@@ -1,8 +1,8 @@
 import { ThemeProvider } from 'styled-components';
 
 import React from 'react';
-import GlobalStyles from './assets/styles/global';
-import defaultTheme from './assets/styles/themes/default';
+import GlobalStyles from '../../assets/styles/global';
+import defaultTheme from '../../assets/styles/themes/default';
 
 function App() {
   return (
