fa72d719f7edd3bc4ad1760a9dfb7ec33005eabb
BrunoAlmeida <bruleorn@gmail.com>
2023-03-14 17:03:46 -0300

    criando os estilos defaults e utilizando o ThemeProvider para ser acessado pelas props

--- mycontacts/fe/src/App.js
+++ mycontacts/fe/src/App.js
@@ -1,12 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+
 import React from 'react';
 import GlobalStyles from './assets/styles/global';
+import defaultTheme from './assets/styles/themes/default';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
       <h1> MyContacts </h1>
-    </>
+    </ThemeProvider>
 
   );
 }
--- mycontacts/fe/src/assets/styles/global.js
+++ mycontacts/fe/src/assets/styles/global.js
@@ -6,6 +6,14 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     font-family: 'Sora', sans-serif;
+  }
 
+  body {
+    background: ${({ theme }) => theme.backgroundColor};
+    font-size: 16px
+  }
+
+  button {
+    cursor: pointer;
   }
 `;
