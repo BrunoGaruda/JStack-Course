e518abb64153ca1183ebffa12a06f97fac034bc6
BrunoAlmeida <bruleorn@gmail.com>
2023-03-15 12:29:23 -0300

    criando o estilo do App, e criando o componente Header

--- mycontacts/fe/src/components/App/index.js
+++ mycontacts/fe/src/components/App/index.js
@@ -3,12 +3,16 @@ import { ThemeProvider } from 'styled-components';
 import React from 'react';
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
+import { Container } from './styles';
+import Header from '../Header';
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <h1> MyContacts </h1>
+      <Container>
+        <Header />
+      </Container>
     </ThemeProvider>
 
   );
