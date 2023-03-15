535a71406b3d3704f5e3631f26cc8242ad31c7e8
BrunoAlmeida <bruleorn@gmail.com>
2023-03-15 18:26:25 -0300

    adição das cores primárias ao default

--- mycontacts/fe/src/assets/styles/themes/default.js
+++ mycontacts/fe/src/assets/styles/themes/default.js
@@ -1,4 +1,12 @@
 export default {
-  // backgroundColor: '#f6f5fc',
-  backgroundColor: '#ccc',
+  colors: {
+    background: '#f6f5fc',
+    primary: {
+      lighter: '#E0E3FF',
+      light: '#6674F4',
+      main: '#5061fc',
+      dark: '#3346F0',
+    },
+  },
+
 };
