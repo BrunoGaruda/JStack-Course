b1010d9b1f8be60627dee7bf52148973b5dc02f1
BrunoAlmeida <bruleorn@gmail.com>
2023-03-21 20:07:44 -0300

    atribuição dos Links dos botões Novo contatot e edict

--- mycontacts/fe/src/pages/Home/index.js
+++ mycontacts/fe/src/pages/Home/index.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import {
   Container, InputSearchContainer, Header, ListContainer, Card,
 } from './styles';
@@ -15,7 +17,7 @@ export default function Home() {
 
       <Header>
         <strong>3 contatos</strong>
-        <a href="/">Novo contato</a>
+        <Link to="/new">Novo contato</Link>
       </Header>
 
       <ListContainer>
@@ -37,52 +39,15 @@ export default function Home() {
           <span>(84) 99999-9999 </span>
         </div>
         <div className="actions">
-          <a href="/">
+          <Link to="/edit/123">
             <img src={edit} alt="Edit" />
-          </a>
+          </Link>
           <button type="button">
             <img src={trash} alt="Delete" />
           </button>
         </div>
       </Card>
 
-      <Card>
-        <div className="info">
-          <div className="contact-name">
-            <strong>Bruno Almeida</strong>
-            <small>instagram</small>
-          </div>
-          <span>bruno@gmail.com </span>
-          <span>(84) 99999-9999 </span>
-        </div>
-        <div className="actions">
-          <a href="/">
-            <img src={edit} alt="Edit" />
-          </a>
-          <button type="button">
-            <img src={trash} alt="Delete" />
-          </button>
-        </div>
-      </Card>
-
-      <Card>
-        <div className="info">
-          <div className="contact-name">
-            <strong>Bruno Almeida</strong>
-            <small>instagram</small>
-          </div>
-          <span>bruno@gmail.com </span>
-          <span>(84) 99999-9999 </span>
-        </div>
-        <div className="actions">
-          <a href="/">
-            <img src={edit} alt="Edit" />
-          </a>
-          <button type="button">
-            <img src={trash} alt="Delete" />
-          </button>
-        </div>
-      </Card>
     </Container>
   );
 }
