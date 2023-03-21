5c5b44378b4290b09e03cbaf91c2558a975ec606
BrunoAlmeida <bruleorn@gmail.com>
2023-03-21 20:09:32 -0300

    atribuindo buttonLabel como props para os botões Cadastrar e Salvar contato

--- mycontacts/fe/src/pages/EditContact/index.js
+++ mycontacts/fe/src/pages/EditContact/index.js
@@ -1,7 +1,11 @@
 import PageHeader from '../../components/PageHeader';
+import ContactForm from '../../components/ContactForm';
 
 export default function EditContact() {
   return (
-    <PageHeader title="Bruno Almeida" />
+    <>
+      <PageHeader title="Bruno Almeida" />
+      <ContactForm buttonLabel="Salvar alterações" />
+    </>
   );
 }
