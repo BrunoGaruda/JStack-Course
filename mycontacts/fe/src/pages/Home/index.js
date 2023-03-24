f00fc3c33becf04653c5b2c8dccd3aac2287fe65
BrunoAlmeida <bruleorn@gmail.com>
2023-03-23 22:04:25 -0300

    criando o loader usando keyframes do css

--- mycontacts/fe/src/pages/Home/index.js
+++ mycontacts/fe/src/pages/Home/index.js
@@ -7,15 +7,17 @@ import {
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-import Modal from '../../components/Modal';
+// import Modal from '../../components/Modal';
+import Loader from '../../components/Loader';
 
 export default function Home() {
   return (
     <Container>
-      <Modal
+      <Loader />
+      {/* <Modal
         // danger={false}
         danger
-      />
+      /> */}
 
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome" />
