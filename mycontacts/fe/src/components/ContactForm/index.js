43a300bfba1227e553a1c0d6e6b236a055e70d0c
BrunoAlmeida <bruleorn@gmail.com>
2023-03-29 19:14:26 -0300

    Incluindo o useState aos Controlled Components, e alterando o title do html para MyContacts

--- mycontacts/fe/src/components/ContactForm/index.js
+++ mycontacts/fe/src/components/ContactForm/index.js
@@ -7,35 +7,46 @@ import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 
-// Controlled components
-// Uncontrolled components
 export default function ContactForm({ buttonLabel }) {
   const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
 
   return (
     <Form>
       <FormGroup>
         <Input
-          value={name}
           placeholder="Nome"
-          // monitorando do evento
+          value={name}
           onChange={(event) => setName(event.target.value)}
         />
       </FormGroup>
 
-      <FormGroup
-        error="O formato do e-mail é inválido."
-      >
-        <Input placeholder="E-mail" error />
+      <FormGroup>
+        <Input
+          placeholder="E-mail"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
+          <option value="discord">Discord</option>
         </Select>
       </FormGroup>
 
