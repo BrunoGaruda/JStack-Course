6f174f2c8087de7bdf9b0f9ee45c24432c47bcc2
BrunoAlmeida <bruleorn@gmail.com>
2023-05-15 16:56:46 -0300

    fazendo o parse do body baseado no Content-Type

--- mycontacts/fe/src/services/utils/HttpClient.js
+++ mycontacts/fe/src/services/utils/HttpClient.js
@@ -10,12 +10,18 @@ class httpClient {
 
     const response = await fetch(`${this.baseUrl}${path}`);
 
-    // status 200 a 299
-    if (response.ok) {
-      return response.json();
+    let body = null;
+    const contentType = response.headers.get('content-type');
+    if (contentType.includes('application/json')) {
+      body = await response.json();
     }
 
-    throw new Error(`${response.status} - ${response.statusText}`);
+    // status ok de 200 a 299
+    if (response.ok) {
+      return body;
+    }
+
+    throw new Error(body.error);
   }
 }
 
