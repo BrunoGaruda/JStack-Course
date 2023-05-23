5dd763251bfa34725907f21fb11e6d9b51f66ab2
BrunoAlmeida <bruleorn@gmail.com>
2023-05-23 18:08:28 -0300

    lidando com o click do botão Tentar novamente

--- mycontacts/fe/src/pages/Home/index.js
+++ mycontacts/fe/src/pages/Home/index.js
@@ -31,21 +31,22 @@ export default function Home() {
     contact.name.toLowerCase().includes(searchTerm.toLowerCase())
   )), [contacts, searchTerm]);
 
-  useEffect(() => {
-    async function loadContacts() {
-      try {
-        setIsLoading(true);
+  async function loadContacts() {
+    try {
+      setIsLoading(true);
 
-        const contactsList = await ContactsService.listContacts(orderBy);
+      const contactsList = await ContactsService.listContacts(orderBy);
 
-        setContacts(contactsList);
-      } catch (error) {
-        setHasError(true);
-      } finally {
-        setIsLoading(false);
-      }
+      setHasError(false);
+      setContacts(contactsList);
+    } catch (error) {
+      setHasError(true);
+    } finally {
+      setIsLoading(false);
     }
+  }
 
+  useEffect(() => {
     loadContacts();
   }, [orderBy]);
 
@@ -55,6 +56,9 @@ export default function Home() {
 
   function handleChangeSearchTerm(event) {
     setSearchTerm(event.target.value);
+  }
+  function handleTryAgain() {
+    loadContacts();
   }
 
   // console.log({ searchTerm });
@@ -89,52 +93,57 @@ export default function Home() {
             <strong>
               Ocorreu um error ao obter os contatos!
             </strong>
-            <Button type="button">
+            <Button type="button" onClick={handleTryAgain}>
               Tentar novamente
             </Button>
           </div>
         </ErrorContainer>
       )}
 
-      {filteredContacts.length > 0 && (
-      <ListHeader orderBy={orderBy}>
-        <button type="button" onClick={handleToggleOrderBy}>
-          <span>Nome</span>
-          <img src={arrow} alt="Arrow" />
-        </button>
-      </ListHeader>
-      )}
+      {!hasError && (
+      <>
+        {filteredContacts.length > 0 && (
+        <ListHeader orderBy={orderBy}>
+          <button type="button" onClick={handleToggleOrderBy}>
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </ListHeader>
+        )}
 
-      {filteredContacts.map((contact) => (
-        <Card key={contact.id}>
-          <div className="info">
-            <div className="contact-name">
-              <strong>{contact.name}</strong>
+        {filteredContacts.map((contact) => (
+          <Card key={contact.id}>
+            <div className="info">
+              <div className="contact-name">
+                <strong>{contact.name}</strong>
 
-              {/* retirando a "bolinha" do contato caso n tenha category */}
-              {contact.category_name && (
-              <small>
-                {contact.category_name}
-              </small>
-              )}
+                {/* retirando a "bolinha" do contato caso n tenha category */}
+                {contact.category_name && (
+                <small>
+                  {contact.category_name}
+                </small>
+                )}
+              </div>
+              <span>
+                {contact.email}
+              </span>
+              <span>
+                {contact.phone}
+              </span>
             </div>
-            <span>
-              {contact.email}
-            </span>
-            <span>
-              {contact.phone}
-            </span>
-          </div>
-          <div className="actions">
-            <Link to={`/edit/${contact.id}`}>
-              <img src={edit} alt="Edit" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
-      ))}
+            <div className="actions">
+              <Link to={`/edit/${contact.id}`}>
+                <img src={edit} alt="Edit" />
+              </Link>
+              <button type="button">
+                <img src={trash} alt="Delete" />
+              </button>
+            </div>
+          </Card>
+        ))}
+      </>
+
+      )}
     </Container>
   );
 }
