d40da67da9f924655c4b018187d80a97ff0d0c1f
BrunoAlmeida <bruleorn@gmail.com>
2023-06-06 17:37:21 -0300

    criando uma constante contacts para receber o category_id com categoryId

--- mycontacts/fe/src/pages/NewContact/index.js
+++ mycontacts/fe/src/pages/NewContact/index.js
@@ -1,9 +1,19 @@
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import ContactsService from '../../services/ContactsService';
 
 export default function NewContact() {
-  function handleSubmit(formData) {
-    console.log('NewContact -> handleSubmit', { formData });
+  async function handleSubmit(formData) {
+    const contact = {
+      name: formData.contact.name,
+      email: formData.contact.email,
+      phone: formData.contact.phone,
+      category_id: formData.contact.categoryId,
+    };
+
+    const response = await ContactsService.createContact(contact);
+
+    console.log(response);
   }
 
   return (
