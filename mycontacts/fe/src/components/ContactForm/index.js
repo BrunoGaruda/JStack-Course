0b19521d375e5e7b92121d45d199e13561f1506d
BrunoAlmeida <bruleorn@gmail.com>
2023-06-14 15:07:25 -0300

    foi adicionado o disabled ao input quando estiver carregando

--- mycontacts/fe/src/components/ContactForm/index.js
+++ mycontacts/fe/src/components/ContactForm/index.js
@@ -99,6 +99,7 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
           placeholder="Nome *"
           value={name}
           onChange={handleNameChange}
+          disabled={isSubmitting}
         />
       </FormGroup>
 
@@ -109,6 +110,7 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
           placeholder="E-mail"
           value={email}
           onChange={handleEmailChange}
+          disabled={isSubmitting}
         />
       </FormGroup>
 
@@ -118,6 +120,7 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
           value={phone}
           onChange={handlePhoneChange}
           maxLength="15"
+          disabled={isSubmitting}
         />
       </FormGroup>
 
@@ -125,7 +128,7 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
         <Select
           value={categoryId}
           onChange={(event) => setCategoryId(event.target.value)}
-          disabled={isLoadingCategories}
+          disabled={isLoadingCategories || isSubmitting}
         >
           <option value="">Sem categoria</option>
 
@@ -141,9 +144,13 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
       </FormGroup>
 
       <ButtonContainer>
-        <Button type="submit" disabled={!isFormValid}>
+        <Button
+          type="submit"
+          disabled={!isFormValid}
+          isLoading={isSubmitting}
+        >
           {!isSubmitting && buttonLabel}
-          {isSubmitting && <Spinner />}
+          {isSubmitting && <Spinner size={16} />}
         </Button>
       </ButtonContainer>
 
