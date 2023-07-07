926d9a06ab9e04a12cb67d691063c9623a332232
BrunoAlmeida <bruleorn@gmail.com>
2023-07-06 23:51:29 -0300

    retirando o async pois os métodos ja são promessas, criando o getContactById método, e criando o useEffect para carregar os dados do contato de edição

--- mycontacts/fe/src/pages/EditContact/index.js
+++ mycontacts/fe/src/pages/EditContact/index.js
@@ -1,11 +1,39 @@
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import ContactsService from '../../services/ContactsService';
 
 export default function EditContact() {
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(
+          id,
+        );
+        console.log({ contactData });
+      } catch {}
+    }
+
+    loadContact();
+  }, [id]);
+
+  function handleSubmit() {
+    //
+  }
+
   return (
     <>
-      <PageHeader title="Bruno Almeida" />
-      <ContactForm buttonLabel="Salvar alterações" />
+      <PageHeader
+        title="Bruno Almeida"
+      />
+
+      <ContactForm
+        buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
--- mycontacts/fe/src/services/ContactsService.js
+++ mycontacts/fe/src/services/ContactsService.js
@@ -5,16 +5,20 @@ class ContactsService {
     this.httpClient = new HttpClient('http://localhost:3001');
   }
 
-  async listContacts(orderBy = 'asc') {
+  listContacts(orderBy = 'asc') {
     return this.httpClient.get(`/contacts?orderBy=${orderBy}`, {
-      headers: {
-        Authorization: 'meutoken',
-        OutroHeader: 'valor',
-      },
+      // headers: {
+      //   Authorization: 'meutoken',
+      //   OutroHeader: 'valor',
+      // },
     });
   }
 
-  async createContact(contact) {
+  getContactById(id) {
+    return this.httpClient.get(`/contacts/${id}`);
+  }
+
+  createContact(contact) {
     return this.httpClient.post('/contacts', { body: contact });
   }
 }
