2f40d5cc773f89a872faa07cd003e711a8d721c2
BrunoAlmeida <bruleorn@gmail.com>
2023-07-19 13:34:01 -0300

    criando o loading no botao de deletar

--- mycontacts/fe/src/components/Modal/index.js
+++ mycontacts/fe/src/components/Modal/index.js
@@ -7,6 +7,7 @@ import Button from '../Button';
 export default function Modal({
   danger,
   visible,
+  isLoading,
   title,
   children,
   cancelLabel,
@@ -32,6 +33,7 @@ export default function Modal({
             type="button"
             className="cancel-button"
             onClick={onCancel}
+            disabled={isLoading}
           >
             {cancelLabel}
 
@@ -40,6 +42,7 @@ export default function Modal({
             type="button"
             danger={danger}
             onClick={onConfirm}
+            isLoading={isLoading}
           >
             {confirmLabel}
           </Button>
@@ -55,6 +58,7 @@ export default function Modal({
 Modal.propTypes = {
   danger: PropTypes.bool,
   visible: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool,
   title: PropTypes.string,
   children: PropTypes.node.isRequired,
   cancelLabel: PropTypes.string,
@@ -65,6 +69,7 @@ Modal.propTypes = {
 
 Modal.defaultProps = {
   danger: false,
+  isLoading: false,
   title: true,
   cancelLabel: 'Cancelar',
   confirmLabel: 'Confirmar',
